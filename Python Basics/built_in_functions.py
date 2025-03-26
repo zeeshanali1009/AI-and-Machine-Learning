485917e0efaa7e694643859536d57d36f2e99ccf
zeeshan1009 <zeeshan10091009@gmail.com>
2025-03-26 10:20:30 +0500

    loops

---
+++ Python Basics/built_in_functions.py
@@ -0,0 +1,17 @@
+# List have ordered,indexed,changable can have duplications
+thislist = ["student","teacher","laborer"]
+print(thislist)
+# List have ordered,indexed,changable can have duplications
+thistuple = ("student","teacher","laborer")
+print(thistuple)
+# List have ordered,indexed,changable can have duplications
+thisset = {"student",12,12,23,34,"teacher","man"}
+print(thisset)
+# List have ordered,indexed,changable can have duplications
+thisdirectory ={
+    "brand": "Ford",
+    "model": "Mustang",
+    "year": 1986
+}
+print(thisdirectory)
+print(["year"])
