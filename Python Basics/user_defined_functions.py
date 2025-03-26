c853d1b771588f09cc78f6efad02cb03c8561555
zeeshan1009 <zeeshan10091009@gmail.com>
2025-03-26 13:59:42 +0500

    functions

---
+++ Python Basics/user_defined_functions.py
@@ -0,0 +1,14 @@
+def sum (a,b):
+    result  = a+b
+    return result
+result = sum(12,12)
+print("The sum of two numbers is ", result)
+
+def greetings(name="World"):
+    print("Hello",name)
+
+greetings()
+greetings("Ali")
+
+
+    
