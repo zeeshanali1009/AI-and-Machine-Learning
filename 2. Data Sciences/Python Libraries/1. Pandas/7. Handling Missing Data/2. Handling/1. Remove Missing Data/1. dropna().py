884069799fcd7535ba44feddbda887b8df2b1690
zeeshan1009 <zeeshan10091009@gmail.com>
2025-07-15 02:17:20 +0500

    data missing and handling

---
+++ 2. Data Sciences/Python Libraries/1. Pandas/7. Handling Missing Data/2. Handling/1. Remove Missing Data/1. dropna().py
@@ -0,0 +1,22 @@
+# dropna method removes the rows where any value is missing hence data becomes inconsistant
+import pandas as pd
+
+data = {
+    "Name": ["Babar Azam", "Saim Ayub", "Muhammaad Haris", "Salman Agha", None, "Irfan Khan Niazi", 
+             "Shadab Khan", "Shaheen Afridi", "Haris Rauf", "Naseem Shah", "Sufyan Muqeem"],
+    "Age": [30, 21, 19, 30, 32, None, 30, 24, 30, 21, 24],
+    "Score": [56, 17, 45, 67, 12, 45, 12, 3, 4, None, 0],
+    "Wickets": [0, 3, 0, 5, 78, 12, 56, 78, 89, 12, 89]
+}
+
+df = pd.DataFrame(data)
+
+#  Show missing data
+print("Missing values per column:")
+print(df.isnull().sum())
+
+#  Drop rows where any value is missing
+clean_df = df.dropna()
+
+print("DataFrame after dropping rows with any missing value:")
+print(clean_df)
