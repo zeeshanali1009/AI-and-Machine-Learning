73b9004a02a16a0f18edaf8631e0a7bed6dca0a9
zeeshan1009 <zeeshan10091009@gmail.com>
2025-07-14 22:41:42 +0500

    file saving

--- 2. Data Sciences/Python Libraries/Pandas/1. File Reading/reading_files.py
+++ 2. Data Sciences/Python Libraries/Pandas/1. File Reading/reading_files.py
@@ -7,9 +7,11 @@ import pandas as pd
 # print(df)
 
 
-
 df = pd.read_excel(r"C:\Users\Zeeshan Ali\Desktop\AI-and-Machine-Learning\2. Data Sciences\Python Libraries\Pandas\1. File Reading\excel_file.xls")
 
 print(df)
+
+# for reading the files placed in the cloud storage we can use the additional library naming as the "gcsfs"
+
 
 
