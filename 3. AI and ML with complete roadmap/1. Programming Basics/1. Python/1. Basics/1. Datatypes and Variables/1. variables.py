e4b91004d3dac6721b6cd82ea4754ea9a4f399b1
zeeshan1009 <zeeshan10091009@gmail.com>
2025-08-06 22:49:53 +0500

    basics

---
+++ 3. AI and ML with complete roadmap/1. Programming Basics/1. Python/1. Basics/1. Datatypes and Variables/1. variables.py
@@ -0,0 +1,12 @@
+# Variables are like the containers which are used to store the data into them. this data can be of variety of forms and the specific 
+# form or type of the data is stored into the specific or relevant container or variable 
+number = 34
+name =  "zeeshan"
+
+# here it is evident that the containers are used to store the data into them whatever the datatype is. Actualy the python automatically 
+# detects the datatype of the variable by looking into the data which is stored into the variables. 
+# but if you can add the manual datatype into the variables this can be done as 
+
+number: int  = 34
+name: str  =  "zeeshan"
+
---
+++ 3. AI and ML with complete roadmap/1. Programming Basics/1. Python/1. Basics/1. Datatypes and Variables/2. datatypes.py
@@ -0,0 +1,29 @@
+# datatype is the type of the variable like it represents the type so that the only that type of data is stored into the variables.
+# Following are the different datatypes:
+# Numeric Data (int, float, complex)
+# String Data (str) 
+# Boolean Data (bool)
+# Sequential Data (list(mutable), tuple(not mutable))
+# Map Data (dictionary)
+
+# examples
+# numeric datatype
+int_number = 12
+float_number = 12.12
+complex_number = complex(12,12)
+
+# string datatype 
+string_data = "Zeeshan"
+
+# boolean datatype 
+isvalid   = True
+
+# sequential datatype
+list = [12,2,35,5,23,34, "Zeeshan", [643,34,35,46,5]]
+tuple = ((12,23,34,45,46,56), "Zeeshan", (13,24.35,45.56,56))
+
+# map datatype
+this_dictionary = {"name" : "Zeeshan", "Age": 21, "Addresss": "Lahore"
+}
+
+
