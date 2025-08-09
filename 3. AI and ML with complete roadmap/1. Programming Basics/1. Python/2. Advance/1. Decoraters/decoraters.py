a81a724ca5d8f06cb781c25a74ea17e4c579bf17
zeeshan1009 <zeeshan10091009@gmail.com>
2025-08-09 10:06:49 +0500

    decoraters

---
+++ 3. AI and ML with complete roadmap/1. Programming Basics/1. Python/2. Advance/1. Decoraters/decoraters.py
@@ -0,0 +1,36 @@
+
+# Decorator function
+# def greet(fx):
+#     def mfx():
+#         print("This is the start.")  # before calling the original function
+#         fx()                         # call original function
+#         print("This is the end.")    # after calling the original function
+#     return mfx                      # return the wrapper function
+
+
+# for the parameterized functions it would be like 
+#  Decorator function
+def greet(fx):
+    def mfx(*arg, **kwargs):
+        print("This is the start.")  
+        result = fx(*arg, **kwargs)                         
+        print("This is the end.") 
+        return result   
+    return mfx                      
+# Using decorator syntax
+# @greet
+# def hlo():
+#     print("Hello!")
+
+# Basic function
+@greet
+def add(a, b):
+    return a + b
+
+# Call the decorated function
+# hlo()
+res = add(12,12)
+print("Result = ", res)
+
+# Or call decorator manually
+# greet(hlo)()
